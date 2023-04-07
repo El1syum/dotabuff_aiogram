257b0c55b373fd66467b6e521e8eb953a45f1d95
el1syum <kilmol606@gmail.com>
2023-04-07 14:18:44 +0300

    updated buttons

--- bot/buttons/common/menu.py
+++ bot/buttons/common/menu.py
@@ -3,10 +3,9 @@ from aiogram import types
 
 def menu_buttons():
     kb = [
-        [types.KeyboardButton(text='1')],
-        [types.KeyboardButton(text='2')],
-        [types.KeyboardButton(text='3')],
-        [types.KeyboardButton(text='4')],
+        [types.KeyboardButton(text='1'), types.KeyboardButton(text='2'),
+         types.KeyboardButton(text='3'), types.KeyboardButton(text='4')],
+
         [types.KeyboardButton(text='Main menu')],
     ]
     keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True,
